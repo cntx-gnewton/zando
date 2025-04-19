13bdb48fbe2c1b98670036b53e0bc9cea9b41868
greyson <greysonnewton@cosnetix.com>
2025-04-19 13:55:05 -0500

    claude is about to take over

--- zando/frontend/src/index.tsx
+++ zando/frontend/src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {GoogleOAuthProvider} from "@react-oauth/google";
 
+const CLIENT_ID = "272043323727-8l5dj2bol9gsa2lg3qkdjijsb0vcjisj.apps.googleusercontent.com"
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <GoogleOAuthProvider clientId={CLIENT_ID}>
+        <App />
+      </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
--- zando/frontend/src/pages/Home.tsx
+++ zando/frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaFileMedical, FaDna, FaFileAlt } from 'react-icons/fa';
+import { GoogleLogin } from "@react-oauth/google";
 
 const HomePage: React.FC = () => {
   return (
